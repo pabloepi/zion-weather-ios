eb59c276349bd14f594e111194dbbfe674e254e1
Pablo Episcopo <pabloepi14@gmail.com>
2019-07-24 15:40:14 -0300

    folders for files sorting, plist file path modification and measurement fix

--- zion-code-test/models/Weather.swift
+++ zion-code-test/models/Weather.swift
@@ -25,7 +25,7 @@ struct Weather {
         humidity = main["humidity"] as? Int
         if let temperature = main["temp"] as? Double {
             let formatter = MeasurementFormatter()
-            let measurement = Measurement(value: temperature, unit: UnitTemperature.celsius)
+            let measurement = Measurement(value: temperature, unit: UnitTemperature.fahrenheit)
             temp = formatter.string(from: measurement)
         } else {
             temp = "no temperature"
